6d0305c18171cba8740f0141008828fb1a3834a9
pablocoop <pabloosorionavarro@gmail..com>
2025-02-13 12:10:52 -0300

    FEAT implemented models relations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image_path',
+        'is_published',
+        'category_id',
+
+    ];
+    //Relacion uno a muchos inversa
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    //Relacion muchos a muchos
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    //Relacion muchos a muchos
+    public function posts(){
+        return $this->belongsTo(Post::class);
+    }
 }
